487f13c32d6f037f3e839f29f0396e924e367be4
Hammad <Hammad@DESKTOP-0NAAQET>
2023-05-22 13:01:55 +0500

    commit

--- src/groupvbse203049_bse201044/GroupVbse203049_bse201044.java
+++ src/groupvbse203049_bse201044/GroupVbse203049_bse201044.java
@@ -2,9 +2,29 @@
 package groupvbse203049_bse201044;
 
 public class GroupVbse203049_bse201044 {
-
-    public static void main(String[] args) {
+    private int uid;
+    private String username;
+    private String  password;
+    public GroupVbse203049_bse201044(int uid,String username,String password)
+    {
+        this.uid=uid;
+        this.username=username;
+        this.password=password;
+    }
+    public int getuid() {
+        return uid;
+    }
+    public String getusername() {
+        return username;
+    }
+    public String getpassword() {
+        return password;
+    }
+ public static void main(String[] args) 
+    {
         
     }
     
 }
+    
+  
---
+++ src/groupvbse203049_bse201044/user.java
@@ -0,0 +1,11 @@
+
+package groupvbse203049_bse201044;
+import java.util.ArrayList;
+import java.util.List;
+public interface user {
+    
+public interface Userrdoa {
+List<GroupVbse203049_bse201044> getalluser();
+ void adduser(GroupVbse203049_bse201044 user);
+}
+}
